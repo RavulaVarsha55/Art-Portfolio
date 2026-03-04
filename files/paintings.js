b9f0bef526d5d580df5fa8bc100d4ab76eb31cef
Ravula Varsha <ravulavarsha55@gmail.com>
2026-03-03 17:00:43 -0800

    images load

--- files/paintings.js
+++ files/paintings.js
@@ -13,5 +13,96 @@ window.paintings = [
     src: "files/Lotus-Line-Sketch.jpg",
     title: "Lotus Line Sketch",
     date: "12/12/2024" 
+  },
+  {
+    src: "files/Lotus-Line-black-Sketch.PNG",
+    title: "Lotus Line Black Sketch",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Boho-Painting.jpg",
+    title: "Boho Painting",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Window-Painting.jpg",
+    title: "Window Painting",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Colorful-Line-Sketch.jpg",
+    title: "Colorful Line Sketch",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Welcome-home-Hangingcraft.jpg",
+    title: "Welcome Home Hanging Craft",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Mandala-Art.jpg",
+    title: "Mandala Art",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Metallic-Mandala.jpg",
+    title: "Metallic Mandala",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Pichwai-Cow-Lotus-Painting.jpg",
+    title: "Pichwai Cow Lotus Painting",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Mandala-Colorful-Patches-Painting.jpg",
+    title: "Mandala Colorful Patches Painting",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Madhubani-fish-painting.jpg",
+    title: "Madhubani Fish Painting",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Blob-Line-Sketch.jpg",
+    title: "Blob Line Sketch",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Colorful-Mandala.jpg",
+    title: "Colorful Mandala",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Heart-Sketch.jpg",
+    title: "Heart Sketch",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Couple-faceless-Portrait.jpg",
+    title: "Couple Faceless Portrait",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Heart-Tissue-Art.jpg",
+    title: "Heart Tissue Art",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Dudu-Bubu-Painting.jpg",
+    title: "Dudu Bubu Painting",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/LOVE-EvilEye-Painting.jpg",
+    title: "LOVE Evil Eye Painting",
+    date: "12/12/2024" 
+  },
+  {
+    src: "files/Kollam_WoodenBoard.jpg",
+    title: "Kollam Wooden Board",
+    date: "12/12/2024" 
   }
+
 ];
